75481484d06b44ee404d1633c7ed1cb7f3e8beed
Anisha Swain <anisha.swain@yahoo.in>
2018-08-18 20:21:42 +0530

    implementation of create post for solution

--- app/post.js
+++ app/post.js
@@ -37,7 +37,9 @@ router.get('/', auth, function(req, res, next) {
 
 router.post('/create', auth, upload.array('images', 12), function(req, res, next) {
   ///FOR Challenge
+  if(req.body.type_post=="Challenge"){
       console.log(req.files)
+      
       req.files[0].path = req.files[0].path.substr(req.files[0].path.indexOf('/') + 1, req.files[0].path.length - 1);
       if (req.files.length > 1) {
         res.send("Exceeds file limit");
@@ -79,9 +81,49 @@ router.post('/create', auth, upload.array('images', 12), function(req, res, next
           });
         }
       }
-    }); 
+    }
 
 ////FOR SOLUTION
+ else{
+
+
+  req.files[0].path = req.files[0].path.substr(req.files[0].path.indexOf('/') + 1, req.files[0].path.length - 1);
+  req.files[1].path = req.files[1].path.substr(req.files[1].path.indexOf('/') + 1, req.files[1].path.length - 1);
+  if (req.files.length > 2) {
+    res.send("Exceeds file limit");
+  } else {
+    if (!req.body.description || !req.body.location || !req.files[0].path ||
+      !req.files[1].path || !req.body.stake_holders || !req.body.heading) {
+      res.status(502).send("Insufficient Field Values")
+    } else {
+      var new_post = new Post({
+        description: req.body.description,
+        location: req.body.location,
+        stake_holders: req.body.stake_holders,
+        heading: req.body.heading,
+        type_post:req.body.type_post,
+        author: req.session.userid.toString(),
+        author_image:req.session.userimage.toString(),
+        image_before: req.files[0].path,
+        image_after: req.files[1].path,
+        like_count:0,
+        likes:[],
+        author_name:req.session.name
+      });
+      new_post.save(function(err, post) {
+        if (err) {
+          console.log(err);
+          res.send(err)
+        } else {
+              res.send("Post saved succesfully");
+          }
+
+      });
+    }
+  }
+
+}
+});
  
 //export our router
 module.exports = router;
